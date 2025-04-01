2a1c92458cbb334f29efd332db5debc808f5dc56
hyunmin-furiosa <hyunmin.jeong@furiosa.ai>
2025-04-01 06:55:10 +0000

    Use llvm clang source for compiler

--- furiosa-mlir-private/include/furiosa-mlir/Target/Furiosa/Utils.h
+++ furiosa-mlir-private/include/furiosa-mlir/Target/Furiosa/Utils.h
@@ -1,31 +1,62 @@
 #pragma once
 
+#include "clang/CodeGen/CodeGenAction.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/CompilerInvocation.h"
+
 #include "llvm/Object/ELFObjectFile.h"
 #include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Support/TargetSelect.h"
 #include "llvm/Support/raw_ostream.h"
 
 namespace mlir::furiosa {
 
+LogicalResult InitializeAArch64() {
+  LLVMInitializeAArch64TargetInfo();
+  LLVMInitializeAArch64Target();
+  LLVMInitializeAArch64TargetMC();
+  LLVMInitializeAArch64AsmPrinter();
+  return success();
+}
+
 FailureOr<std::string> convertArmCToObject(llvm::Twine filepath) {
+  if (failed(InitializeAArch64())) {
+    llvm::report_fatal_error("Failed to initialize AArch64 target");
+    return failure();
+  }
+
   // Compile the C code
   llvm::Twine filepath_out = filepath + ".o";
-  std::string command = "clang ";
-  command += "--target=aarch64-linux-gnu ";
-  command += "-r ";
-  command += "-nostdlib ";
-  command += "-static ";
-  command += "-O3 ";
-  command += "-fno-builtin ";
-  command += "-fno-zero-initialized-in-bss ";
-  command += "-fno-asynchronous-unwind-tables "; // remove .eh_frame section
-  command += "-fno-omit-frame-pointer ";
-  command += "-Werror ";
-  static constexpr std::uint32_t MAX_STACK_USAGE = 1020 * 1024;
-  command += "-Wframe-larger-than=" + std::to_string(MAX_STACK_USAGE) + " ";
-  command += "-Wl,--build-id=none "; // remove .note.gnu.build-id section
-  command += filepath.str() + " ";
-  command += "-o " + filepath_out.str() + " ";
-  system(command.c_str());
+  auto invocation = std::make_shared<clang::CompilerInvocation>();
+  const char *args[] = {""}; // -v to make verbose
+  clang::CompilerInstance compiler;
+  compiler.createDiagnostics(*llvm::vfs::getRealFileSystem());
+  clang::CompilerInvocation::CreateFromArgs(*invocation, args,
+                                            compiler.getDiagnostics());
+  invocation->getTargetOpts().Triple =
+      "aarch64-unknown-none-elf"; // -triple aarch64-unknown-none-elf
+  invocation->getFrontendOpts().Inputs.clear(); // remove default input '-'
+  invocation->getFrontendOpts().Inputs.push_back(
+      clang::FrontendInputFile(filepath.str(), clang::Language::C)); // filepath
+  invocation->getFrontendOpts().OutputFile =
+      filepath_out.str(); // -o filepath_out
+  invocation->getHeaderSearchOpts().UseStandardSystemIncludes =
+      false; // -nostdsysteminc
+  invocation->getHeaderSearchOpts().AddPath(
+      CLANG_COMPILE_INCLUDE_DIRS, clang::frontend::Angled, false, true); // -I
+  invocation->getCodeGenOpts().OptimizationLevel = 3;                    // -O3
+  compiler.setInvocation(std::move(invocation));
+  std::unique_ptr<clang::FrontendAction> action =
+      std::make_unique<clang::EmitObjAction>();
+  compiler.ExecuteAction(*action);
+
+  // Check if object file exists
+  llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> bufferOrErr =
+      llvm::MemoryBuffer::getFile("kernel.c.o");
+  if (!bufferOrErr) {
+    llvm::report_fatal_error(llvm::Twine("Failed to open file: ") +
+                             bufferOrErr.getError().message());
+  }
 
   return filepath_out.str();
 }
