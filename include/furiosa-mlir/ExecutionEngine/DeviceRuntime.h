d1b17d5d7e969849364893447d6eacc41b2ad9ca
hyunmin-furiosa <hyunmin.jeong@furiosa.ai>
2025-04-02 09:38:16 +0000

    Write arguments and read results

--- include/furiosa-mlir/ExecutionEngine/DeviceRuntime.h
+++ include/furiosa-mlir/ExecutionEngine/DeviceRuntime.h
@@ -1,3 +1,5 @@
 #pragma once
 
-void launchKernel(llvm::StringRef kernel);
+void launchKernel(llvm::StringRef kernel,
+                  llvm::ArrayRef<std::uint32_t> argumentSizes,
+                  llvm::ArrayRef<std::uint32_t> resultSizes);
