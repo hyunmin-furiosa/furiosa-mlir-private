d9b34d5e1bb4189236c785428e3b027936020d09
hyunmin-furiosa <hyunmin.jeong@furiosa.ai>
2025-04-02 07:23:26 +0000

    Use furiosa-torch apis

--- furiosa-mlir-private/lib/ExecutionEngine/DeviceRuntime.cpp
+++ furiosa-mlir-private/lib/ExecutionEngine/DeviceRuntime.cpp
@@ -1,11 +1,26 @@
+#include "furiosa_torch.h"
+#include "llvm/Support/Debug.h"
 #include "llvm/Support/MemoryBuffer.h"
 
-extern "C" std::uint32_t launch_kernel(std::uint8_t chip_id, std::uint8_t pe_id,
-                                       std::uint8_t pe_id_end,
-                                       const std::uint8_t *kernel,
-                                       std::uint32_t size);
-
 void launchKernel(llvm::StringRef kernel) {
-  launch_kernel(0, 0, 0, reinterpret_cast<const uint8_t *>(kernel.data()),
-                kernel.size());
+  // generate pe program
+  std::vector<furiosa_torch::PeProgram *> pe_programs;
+  pe_programs.push_back(
+      furiosa_torch::pe_program_load_inst(0x0, 0x0, kernel.size()));
+  pe_programs.push_back(furiosa_torch::pe_program_launch(0x0, nullptr));
+  auto pe_program =
+      furiosa_torch::pe_program_seq(pe_programs.data(), pe_programs.size());
+
+  // generate hal program
+  std::vector<furiosa_torch::HalProgram *> hal_programs;
+  hal_programs.push_back(furiosa_torch::hal_program_write_at(
+      reinterpret_cast<const std::uint64_t>(kernel.data()), 0x0,
+      kernel.size()));
+  hal_programs.push_back(furiosa_torch::hal_program_execute(pe_program));
+  auto hal_program =
+      furiosa_torch::hal_program_seq(hal_programs.data(), hal_programs.size());
+
+  // initialize device and execute hal program
+  auto device = furiosa_torch::device_new(0, 0, 0);
+  furiosa_torch::device_execute(device, hal_program);
 }
