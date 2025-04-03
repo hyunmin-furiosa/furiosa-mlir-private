b76e1c732161c004d7b0ef391344a84ee72f2e62
hyunmin-furiosa <hyunmin.jeong@furiosa.ai>
2025-04-03 01:42:39 +0000

    Separate read write binaries

--- furiosa-mlir-private/include/furiosa-mlir/ExecutionEngine/DeviceRuntime.h
+++ furiosa-mlir-private/include/furiosa-mlir/ExecutionEngine/DeviceRuntime.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "llvm/ADT/ArrayRef.h"
+
 void launchKernel(llvm::StringRef kernel,
                   llvm::ArrayRef<std::uint32_t> argumentSizes,
                   llvm::ArrayRef<std::uint32_t> resultSizes);
