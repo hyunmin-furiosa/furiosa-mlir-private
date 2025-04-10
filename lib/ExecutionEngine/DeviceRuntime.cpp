307b25102b266516140ca3ae9f72498567198185
hyunmin-furiosa <hyunmin.jeong@furiosa.ai>
2025-04-10 01:41:56 +0000

    Fix io buffer address problem

--- lib/ExecutionEngine/DeviceRuntime.cpp
+++ lib/ExecutionEngine/DeviceRuntime.cpp
@@ -25,10 +25,12 @@ void launchKernel(llvm::StringRef kernel,
   for (auto size : argumentSizes) {
     std::vector<std::uint8_t> argument(size, 1);
     arguments.push_back(argument);
+  }
+  for (auto &argument : arguments) {
     hal_programs.push_back(furiosa_torch::hal_program_write_at(
         reinterpret_cast<const std::uint64_t>(argument.data()), current_address,
-        size));
-    current_address = (current_address + size + 255) / 256 * 256;
+        argument.size()));
+    current_address = (current_address + argument.size() + 255) / 256 * 256;
   }
 
   hal_programs.push_back(furiosa_torch::hal_program_execute(pe_program));
@@ -36,12 +38,14 @@ void launchKernel(llvm::StringRef kernel,
   // read results
   std::vector<std::vector<std::uint8_t>> results;
   for (auto size : resultSizes) {
-    std::vector<std::uint8_t> result(size, 1);
+    std::vector<std::uint8_t> result(size, 0);
     results.push_back(result);
+  }
+  for (auto &result : results) {
     hal_programs.push_back(furiosa_torch::hal_program_read_at(
         current_address, reinterpret_cast<const std::uint64_t>(result.data()),
-        size));
-    current_address = (current_address + size + 255) / 256 * 256;
+        result.size()));
+    current_address = (current_address + result.size() + 255) / 256 * 256;
   }
 
   // initialize device and execute hal program
