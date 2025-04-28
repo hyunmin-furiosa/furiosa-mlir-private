70fc58bda65ac0baed1355b805bc53ef595b5da3
hyunmin-furiosa <hyunmin.jeong@furiosa.ai>
2025-04-28 09:58:29 +0000

    Add -convert-func-to-llvm pass

--- lib/InitAll.cpp
+++ lib/InitAll.cpp
@@ -9,6 +9,8 @@
 
 #include "furiosa-mlir/InitAll.h"
 
+#include "mlir/Conversion/FuncToLLVM/ConvertFuncToLLVM.h"
+#include "mlir/Conversion/Passes.h"
 #include "mlir/Dialect/Complex/IR/Complex.h"
 #include "mlir/Dialect/Func/Extensions/InlinerExtension.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
@@ -20,18 +22,10 @@
 #include "mlir/Dialect/Tensor/IR/Tensor.h"
 #include "mlir/Dialect/Tensor/IR/TensorInferTypeOpInterfaceImpl.h"
 #include "mlir/IR/Dialect.h"
+#include "mlir/Transforms/Passes.h"
 
 #include "furiosa-mlir/Dialect/Furiosa/IR/FuriosaDialect.h"
 #include "furiosa-mlir/Dialect/Furiosa/Transforms/Passes.h"
-
-#ifdef TORCH_MLIR_ENABLE_STABLEHLO
-#include "stablehlo/conversions/linalg/transforms/Passes.h"
-#include "stablehlo/transforms/Passes.h"
-#endif
-
-#ifdef TORCH_MLIR_ENABLE_TOSA
-#include "mlir/Dialect/Tosa/IR/TosaOps.h"
-#endif
 
 void mlir::furiosa::registerAllDialects(mlir::DialectRegistry &registry) {
   registry.insert<mlir::func::FuncDialect>();
@@ -40,23 +34,15 @@ void mlir::furiosa::registerAllDialects(mlir::DialectRegistry &registry) {
 }
 
 void mlir::furiosa::registerAllExtensions(mlir::DialectRegistry &registry) {
-  mlir::func::registerInlinerExtension(registry);
-  tensor::registerInferTypeOpInterfaceExternalModels(registry);
+  registerConvertFuncToLLVMInterface(registry);
 }
 
 void mlir::furiosa::registerAllPasses() {
+  // General passes
+  registerTransformsPasses();
+
+  // Conversion passes
+  registerConvertFuncToLLVMPass();
+
   mlir::furiosa::registerFuriosaPasses();
-
-#ifdef TORCH_MLIR_ENABLE_STABLEHLO
-  mlir::stablehlo::registerStablehloLegalizeToLinalgPass();
-  mlir::stablehlo::registerStablehloAggressiveSimplificationPass();
-  mlir::stablehlo::registerStablehloRefineShapesPass();
-  mlir::stablehlo::registerStablehloConvertToSignlessPass();
-  mlir::stablehlo::registerShapeLegalizeToStablehloPass();
-  mlir::stablehlo::registerStablehloLegalizeDeprecatedOpsPass();
-#endif
-
-#ifdef TORCH_MLIR_ENABLE_REFBACKEND
-  mlir::torch::RefBackend::registerRefBackendPasses();
-#endif
 }
--- tools/furiosa-mlir-opt/furiosa-mlir-opt.cpp
+++ tools/furiosa-mlir-opt/furiosa-mlir-opt.cpp
@@ -26,27 +26,9 @@ using namespace mlir;
 int main(int argc, char **argv) {
   mlir::furiosa::registerAllPasses();
 
-  // Core Transforms
-  registerCanonicalizerPass();
-  registerCSEPass();
-  registerInlinerPass();
-  registerLocationSnapshotPass();
-  registerLoopInvariantCodeMotionPass();
-  registerPrintOpStatsPass();
-  registerViewOpGraphPass();
-  registerStripDebugInfoPass();
-  registerSymbolDCEPass();
-
-  // memref passes used in torch-backend-to-linalg-on-tensors-backend-pipeline
-  memref::registerExpandOpsPass();
-  memref::registerResolveShapedTypeResultDimsPass();
-
   DialectRegistry registry;
   mlir::furiosa::registerAllDialects(registry);
 
-#ifdef TORCH_MLIR_ENABLE_STABLEHLO
-  mlir::stablehlo::registerAllDialects(registry);
-#endif
   return mlir::asMainReturnCode(mlir::MlirOptMain(
       argc, argv, "MLIR modular optimizer driver\n", registry));
 }
