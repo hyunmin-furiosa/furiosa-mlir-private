87057b25eba390e5661369094888839af0e341df
hyunmin-furiosa <hyunmin.jeong@furiosa.ai>
2025-05-30 10:00:17 +0000

    Partially implement contract op lowering

--- lib/Conversion/FuriosaToFuriosaTask/FuriosaToFuriosaTask.cpp
+++ lib/Conversion/FuriosaToFuriosaTask/FuriosaToFuriosaTask.cpp
@@ -156,12 +156,16 @@ LoadTrfOpLowering::matchAndRewrite(furiosa::LoadTrfOp load_trf_op,
   rewriter.setInsertionPoint(load_trf_op);
 
   auto input_base = *getAddress(load_trf_op.getSource());
-  auto tensor_type = llvm::dyn_cast_or_null<RankedTensorType>(
-      load_trf_op.getSource().getType());
-  auto size = tensor_type.getNumElements() *
-              tensor_type.getElementTypeBitWidth() / CHAR_BIT;
-
+  auto input_shape = *getShape(load_trf_op.getSource());
+  auto input_element_size = *getElementSize(load_trf_op.getSource());
+  assert(input_shape.size() == 3 &&
+         "load trf op source should be a 2D tensor with batch");
   auto output_base = *getAddress(load_trf_op.getDestination());
+
+  std::int64_t batch = input_shape[0];
+  std::int64_t dim_a = input_shape[1];
+  std::int64_t dim_b = input_shape[2];
+  std::int64_t size = batch * dim_a * dim_b * input_element_size;
 
   bool target_context = 1; // 0 for main, 1 for sub
   bool context_id = 0;     // always 0 for sub context
@@ -175,29 +179,27 @@ LoadTrfOpLowering::matchAndRewrite(furiosa::LoadTrfOp load_trf_op,
 
   { // set sub fetch unit sfr
     auto sfr = task::sfr::slice::SubFetchUnit<task::sfr_data_t>();
-    std::uint64_t base = input_base;
-    std::uint64_t type_conversion = 0;
-    std::uint64_t num_zero_points = 0;
-    std::uint64_t zero_point0 = 0;
-    std::uint64_t zero_point1 = 0;
-    ArrayAttr limits = rewriter.getI64ArrayAttr({4, 8, 1, 1, 1, 1, 1, 1});
-    ArrayAttr strides = rewriter.getI64ArrayAttr({8, 32, 0, 0, 0, 0, 0, 0});
-    std::uint64_t flit_count = 1;
-    std::uint64_t words_per_packet = SUB_FETCH_WORDS_PER_PACKET;
-    std::uint64_t topology = 0;
-    std::uint64_t outer_slice_log_size = 0;
-    std::uint64_t outer_dim0_log_size = 0;
-    std::uint64_t outer_dim1_log_size = 0;
-    std::uint64_t outer_dim0_chunk_size = 0;
-    std::uint64_t outer_dim1_chunk_size = 0;
-    ArrayAttr custom_snoop_bitmap_mask = nullptr;
-    auto sfr_op = rewriter.create<task::sfr::SfrSubFetchUnitOp>(
-        load_trf_op.getLoc(), base, type_conversion, num_zero_points,
-        zero_point0, zero_point1, limits, strides, flit_count, words_per_packet,
-        topology, outer_slice_log_size, outer_dim0_log_size,
-        outer_dim1_log_size, outer_dim0_chunk_size, outer_dim1_chunk_size,
-        custom_snoop_bitmap_mask);
+    sfr.base = input_base;
+    sfr.limits_element0 = 4;
+    sfr.limits_element1 = 2;
+    sfr.limits_element2 = 2;
+    sfr.limits_element3 = dim_b / 32;
+    sfr.limits_element4 = dim_a / 32;
+    sfr.limits_element5 = 8;
+    sfr.limits_element6 = 1;
+    sfr.limits_element7 = 1;
+    sfr.strides_element0 = 8;
+    sfr.strides_element1 = dim_b;
+    sfr.strides_element2 = dim_b * 16;
+    sfr.strides_element3 = 32;
+    sfr.strides_element4 = dim_b * 32;
+    sfr.strides_element5 = dim_b * 2;
+    sfr.strides_element6 = 0;
+    sfr.strides_element7 = 0;
+    sfr.flit_count = size / FLIT_SIZE;
+    sfr.words_per_packet = SUB_FETCH_WORDS_PER_PACKET;
 
+    auto sfr_op = createSfrSubFetchUnitOp(rewriter, load_trf_op.getLoc(), sfr);
     std::uint64_t sfr_address =
         SFR_BROADCAST + sfr.get_base() + context_id_offset;
     auto mtosfr_op = rewriter.create<task::DynamicMtosfrOp>(
@@ -208,21 +210,14 @@ LoadTrfOpLowering::matchAndRewrite(furiosa::LoadTrfOp load_trf_op,
   { // set tensor register file sfr
     auto sfr =
         task::sfr::slice::DotProductEngineRegisterFile<task::sfr_data_t>();
-    std::uint64_t write_interleaving_flit_count = 0;
-    std::uint64_t write_mode = 0;
-    std::uint64_t write_mac_rows = 8;
-    std::uint64_t write_skip_flit_count = 0;
-    std::uint64_t write_row_base = output_base / TENSOR_REGISTER_FILE_ROW_SIZE;
-    std::uint64_t write_mac_row_interleaving = 0;
-    std::uint64_t write_row_count = 1;
-    std::uint64_t write_flits_per_period = 1;
-    std::uint64_t write_valid_flits_per_period = 1;
-    auto sfr_op = rewriter.create<task::sfr::SfrTensorRegisterFileOp>(
-        load_trf_op.getLoc(), write_interleaving_flit_count, write_mode,
-        write_mac_rows, write_skip_flit_count, write_row_base,
-        write_mac_row_interleaving, write_row_count, write_flits_per_period,
-        write_valid_flits_per_period);
+    sfr.write_mac_rows = MAC_ROWS;
+    sfr.write_row_base = output_base / TENSOR_REGISTER_FILE_ROW_SIZE;
+    sfr.write_row_count = size / MAC_ROWS / TENSOR_REGISTER_FILE_ROW_SIZE;
+    sfr.write_flits_per_period = 1;
+    sfr.write_valid_flits_per_period = 1;
 
+    auto sfr_op =
+        createSfrTensorRegisterFileOp(rewriter, load_trf_op.getLoc(), sfr);
     std::uint64_t sfr_address =
         SFR_BROADCAST + sfr.get_base() + context_id_offset;
     auto mtosfr_op = rewriter.create<task::DynamicMtosfrOp>(
@@ -267,11 +262,24 @@ ContractOpLowering::matchAndRewrite(linalg::ContractOp contract_op,
 
   assert(contract_op.getInputs().size() == 2 &&
          "contract op should have exactly two inputs");
-  auto input0_base = *getAddress(contract_op.getInputs()[0]);
-  auto input1_base = *getAddress(contract_op.getInputs()[1]);
+  auto weight_base = *getAddress(contract_op.getInputs()[0]);
+  auto weight_shape = *getShape(contract_op.getInputs()[0]);
+  assert(weight_shape.size() == 3 &&
+         "contract op weight should be a 2D tensor with batch");
+  auto input_base = *getAddress(contract_op.getInputs()[1]);
+  auto input_shape = *getShape(contract_op.getInputs()[1]);
+  // auto input_element_size = *getElementSize(contract_op.getInputs()[1]);
+  // auto input_element_type = *getElementType(contract_op.getInputs()[1]);
+  assert(input_shape.size() == 3 &&
+         "contract op input should be a 2D tensor with batch");
   assert(contract_op.getOutputs().size() == 1 &&
          "contract op should have exactly one output");
   auto output_base = *getAddress(contract_op.getOutputs()[0]);
+
+  std::int64_t dim_a = weight_shape[1];
+  std::int64_t dim_b = input_shape[1];
+  std::int64_t dim_c = input_shape[2];
+
   bool context_id =
       contract_op->getAttrOfType<BoolAttr>("context_id").getValue();
 
@@ -279,63 +287,38 @@ ContractOpLowering::matchAndRewrite(linalg::ContractOp contract_op,
   std::uint64_t context_id_offset = context_id ? CONTEXT_ID_OFFSET : 0x0;
   std::uint32_t subunit_bitmap = SubUnit::DataMemorySlice | SubUnit::FetchUnit |
                                  SubUnit::DotProductEngine |
-                                 SubUnit::CommitUnit;
-  std::uint64_t route_bitmap =
-      DataPathUnitRoute::DotProductEngine | DataPathUnitRoute::Commit;
+                                 SubUnit::VectorEngine | SubUnit::CommitUnit;
+  std::uint64_t route_bitmap = DataPathUnitRoute::DotProductEngine |
+                               DataPathUnitRoute::VectorEngine |
+                               DataPathUnitRoute::Commit;
 
   SmallVector<Value> mtosfr_ops;
 
   { // set main fetch unit sfr
     auto sfr = task::sfr::slice::FetchUnitMainContext<task::sfr_data_t>();
-    std::uint64_t fetch_mode = 0;
-    std::uint64_t num_zero_points = 0;
-    std::uint64_t zero_point0 = 0;
-    std::uint64_t zero_point1 = 0;
-    std::uint64_t table_entry_size = 0;
-    std::uint64_t tables = 0;
-    std::uint64_t indirect_base = 0;
-    std::uint64_t indirect_dim = 0;
-    std::uint64_t table_base_mode = 0;
-    std::uint64_t indirect_pointer_size = 0;
-    std::uint64_t zeropoint_tail_mode = 0;
-    std::uint64_t last_dim_pad_value = 0;
-    std::uint64_t last_dim = 0;
-    std::uint64_t pad_order = 0;
-    std::uint64_t last_dim_rightmost_valid_count_dim = 0;
-    std::uint64_t last_dim_left_pad_count = 0;
-    std::uint64_t type_conversion = 0;
-    std::uint64_t last_dim_left_pad_mode = 0;
-    std::uint64_t zeropoint_dims = 0;
-    ArrayAttr last_dim_rightmost_valid_count = nullptr;
-    std::uint64_t base = input0_base;
-    std::uint64_t fetch_size = 0;
-    ArrayAttr limits = rewriter.getI64ArrayAttr({1});
-    ArrayAttr strides = rewriter.getI64ArrayAttr({0});
-    std::uint64_t flit_count = 0;
-    std::uint64_t words_per_packet = 0;
-    std::uint64_t zeropoint_fetch_limit = 0;
-    std::uint64_t topology = 0;
-    std::uint64_t channel_config = 0;
-    std::uint64_t outer_slice_log_size = 0;
-    std::uint64_t outer_dim0_log_size = 0;
-    std::uint64_t outer_dim1_log_size = 0;
-    std::uint64_t outer_dim0_chunk_size = 0;
-    std::uint64_t outer_dim1_chunk_size = 0;
-    ArrayAttr custom_snoop_bitmap_mask = nullptr;
 
-    auto sfr_op = rewriter.create<task::sfr::SfrMainFetchUnitOp>(
-        contract_op.getLoc(), fetch_mode, num_zero_points, zero_point0,
-        zero_point1, table_entry_size, tables, indirect_base, indirect_dim,
-        table_base_mode, indirect_pointer_size, zeropoint_tail_mode,
-        last_dim_pad_value, last_dim, pad_order,
-        last_dim_rightmost_valid_count_dim, last_dim_left_pad_count,
-        type_conversion, last_dim_left_pad_mode, zeropoint_dims,
-        last_dim_rightmost_valid_count, base, fetch_size, limits, strides,
-        flit_count, words_per_packet, zeropoint_fetch_limit, topology,
-        channel_config, outer_slice_log_size, outer_dim0_log_size,
-        outer_dim1_log_size, outer_dim0_chunk_size, outer_dim1_chunk_size,
-        custom_snoop_bitmap_mask);
+    sfr.base = input_base;
+    sfr.fetch_size = 16; // to fit contract op
+    sfr.limits_element0 = 2;
+    sfr.limits_element1 = 2;
+    sfr.limits_element2 = 16;
+    sfr.limits_element3 = 2;
+    sfr.limits_element4 = dim_b / 32;
+    sfr.limits_element5 = dim_a / 64;
+    sfr.limits_element6 = dim_c / 32;
+    sfr.limits_element7 = 1;
+    sfr.strides_element0 = dim_c;
+    sfr.strides_element1 = 16;
+    sfr.strides_element2 = dim_c * 2;
+    sfr.strides_element3 = 0;
+    sfr.strides_element4 = dim_c * 32;
+    sfr.strides_element5 = 0;
+    sfr.strides_element6 = 32;
+    sfr.strides_element7 = 0;
+    sfr.flit_count = dim_a * dim_b * dim_c / 32 / FLIT_SIZE;
+    sfr.words_per_packet = 4;
 
+    auto sfr_op = createSfrMainFetchUnitOp(rewriter, contract_op.getLoc(), sfr);
     std::uint64_t sfr_address =
         SFR_BROADCAST + sfr.get_base() + context_id_offset;
     auto mtosfr_op = rewriter.create<task::DynamicMtosfrOp>(
@@ -346,46 +329,65 @@ ContractOpLowering::matchAndRewrite(linalg::ContractOp contract_op,
   { // set dot product engine sfr
     auto sfr =
         task::sfr::slice::DotProductEngineMainContext<task::sfr_data_t>();
-    std::uint64_t reg_indexer_base = 1;
-    std::uint64_t acc_indexer_base = 0;
-    std::uint64_t flits_per_input = 0;
-    std::uint64_t feed_input_transpose = 0;
-    std::uint64_t initial_shift_dim = 0;
-    std::uint64_t shift_stride = 0;
-    std::uint64_t pop_dim = 0;
-    std::uint64_t shift_dim = 0;
-    std::uint64_t channel_config = 0;
-    std::uint64_t feed_data_type = 0;
-    ArrayAttr initial_shift = nullptr;
-    ArrayAttr iter_seq_limits = nullptr;
-    ArrayAttr reg_indexer_strides = rewriter.getI64ArrayAttr({0});
-    ArrayAttr acc_indexer_strides = rewriter.getI64ArrayAttr({0});
-    std::uint64_t acc_limit = 0;
-    std::uint64_t acc_cols = 0;
-    std::uint64_t acc_reset = 0;
-    std::uint64_t output_major = 0;
-    std::uint64_t acc_init_value = 0;
-    std::uint64_t mac_tree_operation = 0;
-    std::uint64_t mac_tree_depth = 0;
-    std::uint64_t mac_type = 0;
-    std::uint64_t mac_rows = 0;
-    std::uint64_t fp_ieee_nan_multiplication = 0;
-    std::uint64_t fxp_shift_rounding_mode = 0;
-    std::uint64_t data_type = 0;
-    std::uint64_t reg_read_log_size = 0;
-    std::uint64_t reg_read_mode = 0;
-    std::uint64_t reg_read_cache_mode = 0;
+    sfr.reg_indexer_base = weight_base;
+    sfr.acc_indexer_base = 0;
+    sfr.flits_per_input = 2;
+    sfr.feed_input_transpose = 1;
+    sfr.initial_shift_dim = 0;
+    sfr.shift_stride = 0;
+    sfr.pop_dim = 1;
+    sfr.shift_dim = 0;
+    sfr.feed_data_type = 2;
+    sfr.iter_seq_limits_element0 = 4;
+    sfr.iter_seq_limits_element1 = 32;
+    sfr.iter_seq_limits_element2 = dim_b / 32;
+    sfr.iter_seq_limits_element3 = dim_a / 64;
+    sfr.iter_seq_limits_element4 = dim_c / 32;
+    sfr.iter_seq_limits_element5 = 1;
+    sfr.iter_seq_limits_element6 = 1;
+    sfr.iter_seq_limits_element7 = 1;
+    sfr.reg_indexer_strides_element0 = 64;
+    sfr.reg_indexer_strides_element1 = 2;
+    sfr.reg_indexer_strides_element2 = 256;
+    sfr.reg_indexer_strides_element3 = 256 * dim_b / 32;
+    sfr.reg_indexer_strides_element4 = 0;
+    sfr.reg_indexer_strides_element5 = 0;
+    sfr.reg_indexer_strides_element6 = 0;
+    sfr.reg_indexer_strides_element7 = 0;
+    sfr.acc_indexer_strides_element0 = 1;
+    sfr.acc_indexer_strides_element1 = 0;
+    sfr.acc_indexer_strides_element2 = 0;
+    sfr.acc_indexer_strides_element3 = 0;
+    sfr.acc_indexer_strides_element4 = 0;
+    sfr.acc_indexer_strides_element5 = 0;
+    sfr.acc_indexer_strides_element6 = 0;
+    sfr.acc_indexer_strides_element7 = 0;
+    sfr.acc_limit = dim_b / 2;
+    sfr.acc_cols = 32;
+    sfr.output_major = 0;
+    sfr.mac_tree_depth = 1;
+    sfr.mac_type = 1;
+    sfr.mac_rows = MAC_ROWS;
+    sfr.data_type = 2;
+    sfr.reg_read_log_size = 1;
 
-    auto sfr_op = rewriter.create<task::sfr::SfrDotProductEngineOp>(
-        contract_op.getLoc(), reg_indexer_base, acc_indexer_base,
-        flits_per_input, feed_input_transpose, initial_shift_dim, shift_stride,
-        pop_dim, shift_dim, channel_config, feed_data_type, initial_shift,
-        iter_seq_limits, reg_indexer_strides, acc_indexer_strides, acc_limit,
-        acc_cols, acc_reset, output_major, acc_init_value, mac_tree_operation,
-        mac_tree_depth, mac_type, mac_rows, fp_ieee_nan_multiplication,
-        fxp_shift_rounding_mode, data_type, reg_read_log_size, reg_read_mode,
-        reg_read_cache_mode);
+    auto sfr_op =
+        createSfrDotProductEngineOp(rewriter, contract_op.getLoc(), sfr);
+    std::uint64_t sfr_address =
+        SFR_BROADCAST + sfr.get_base() + context_id_offset;
+    auto mtosfr_op = rewriter.create<task::DynamicMtosfrOp>(
+        contract_op.getLoc(), sfr_op, ValueRange(), sfr_address);
+    mtosfr_ops.push_back(mtosfr_op);
+  }
 
+  { // set vector engine sfr
+    auto sfr = task::sfr::slice::VectorRouteUnitMainContext<task::sfr_data_t>();
+    sfr.route_info_data_out_source = 1;
+    sfr.cast_compaction_mode = 2;
+    sfr.compaction_mode_cast_compaction_count = 4;
+
+    auto sfr_op =
+        createSfrVectorRouteUnitOp(rewriter, contract_op.getLoc(), sfr);
     std::uint64_t sfr_address =
         SFR_BROADCAST + sfr.get_base() + context_id_offset;
     auto mtosfr_op = rewriter.create<task::DynamicMtosfrOp>(
@@ -395,18 +397,32 @@ ContractOpLowering::matchAndRewrite(linalg::ContractOp contract_op,
 
   { // set main commit unit sfr
     auto sfr = task::sfr::slice::CommitUnitMainContext<task::sfr_data_t>();
-    std::uint64_t type_conversion = 0;
-    std::uint64_t base = output_base;
-    std::uint64_t commit_in_size = 0;
-    std::uint64_t commit_size = 0;
-    ArrayAttr limits = rewriter.getI64ArrayAttr({1});
-    ArrayAttr strides = rewriter.getI64ArrayAttr({1});
-    ArrayAttr slice_enable_bitmap_mask = nullptr;
+    sfr.base = output_base;
+    sfr.commit_in_size = 32;
+    sfr.commit_size = 32;
+    sfr.limits_element0 = dim_a;
+    sfr.limits_element1 = dim_c / 32;
+    sfr.limits_element2 = 1;
+    sfr.limits_element3 = 1;
+    sfr.limits_element4 = 1;
+    sfr.limits_element5 = 1;
+    sfr.limits_element6 = 1;
+    sfr.limits_element7 = 1;
+    sfr.strides_element0 = dim_c;
+    sfr.strides_element1 = 32;
+    sfr.strides_element2 = 0;
+    sfr.strides_element3 = 0;
+    sfr.strides_element4 = 0;
+    sfr.strides_element5 = 0;
+    sfr.strides_element6 = 0;
+    sfr.strides_element7 = 0;
+    sfr.commit_unit_slice_enable_bitmap0 = 0xffffffffffffffff;
+    sfr.commit_unit_slice_enable_bitmap1 = 0xffffffffffffffff;
+    sfr.commit_unit_slice_enable_bitmap2 = 0xffffffffffffffff;
+    sfr.commit_unit_slice_enable_bitmap3 = 0xffffffffffffffff;
 
-    auto sfr_op = rewriter.create<task::sfr::SfrMainCommitUnitOp>(
-        contract_op.getLoc(), type_conversion, base, commit_in_size,
-        commit_size, limits, strides, slice_enable_bitmap_mask);
-
+    auto sfr_op =
+        createSfrMainCommitUnitOp(rewriter, contract_op.getLoc(), sfr);
     std::uint64_t sfr_address =
         SFR_BROADCAST + sfr.get_base() + context_id_offset;
     auto mtosfr_op = rewriter.create<task::DynamicMtosfrOp>(
