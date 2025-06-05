6667fb6e2b75df5420b5bf1e8103527a09840713
hyunmin-furiosa <hyunmin.jeong@furiosa.ai>
2025-06-05 08:07:26 +0000

    Test python binding for furiosa dialect

--- furiosa-mlir-private/lib/Bindings/Python/RegisterEverything.cpp
+++ furiosa-mlir-private/lib/Bindings/Python/RegisterEverything.cpp
@@ -3,12 +3,13 @@
 #include "mlir/Bindings/Python/Nanobind.h"
 #include "mlir/Bindings/Python/NanobindAdaptors.h"
 
-NB_MODULE(_furiosaMlirRegistration, m) {
+NB_MODULE(_mlirRegisterEverything, m) {
   m.doc() = "Furiosa-MLIR All Dialects, Translations and Passes Registration";
 
   m.def("register_dialects", [](MlirDialectRegistry registry) {
     furiosaMlirRegisterAllDialects(registry);
   });
+  m.def("register_llvm_translations", [](MlirContext context) {});
 
   // Register all passes on load.
   furiosaMlirRegisterAllPasses();
