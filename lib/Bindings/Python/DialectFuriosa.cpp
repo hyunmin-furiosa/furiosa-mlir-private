89db452053c103e56c5e43928b992b7fb84b9a5f
hyunmin-furiosa <hyunmin.jeong@furiosa.ai>
2025-06-10 08:16:12 +0000

    Implement furiosa attributes

--- lib/Bindings/Python/DialectFuriosa.cpp
+++ lib/Bindings/Python/DialectFuriosa.cpp
@@ -27,4 +27,39 @@ NB_MODULE(_furiosaMlirDialectsFuriosa, m) {
             return cls(mlirFuriosaBufferTypeGet(context));
           },
           nb::arg("cls"), nb::arg("context").none() = nb::none());
+
+  mlir_attribute_subclass(m, "TargetAttr", mlirAttributeIsAFuriosaTargetAttr)
+      .def_classmethod(
+          "get",
+          [](const nb::object &cls, MlirContext context, std::uint64_t npu,
+             std::uint64_t peBegin, std::uint64_t peEnd) {
+            return cls(mlirFuriosaTargetAttrGet(context, npu, peBegin, peEnd));
+          },
+          nb::arg("cls"), nb::arg("context").none() = nb::none(),
+          nb::arg("npu"), nb::arg("pe_begin"), nb::arg("pe_end"));
+
+  mlir_attribute_subclass(m, "MappingAttr", mlirAttributeIsAFuriosaMappingAttr)
+      .def_classmethod(
+          "get",
+          [](const nb::object &cls, MlirContext context) {
+            return cls(mlirFuriosaMappingAttrGet(context));
+          },
+          nb::arg("cls"), nb::arg("context").none() = nb::none());
+
+  nb::enum_<mlir::furiosa::MemoryType>(m, "MemoryType")
+      .value("dram", mlir::furiosa::MemoryType::dram)
+      .value("sram", mlir::furiosa::MemoryType::sram)
+      .value("trf", mlir::furiosa::MemoryType::trf)
+      .value("vrf", mlir::furiosa::MemoryType::vrf);
+
+  mlir_attribute_subclass(m, "MemoryTypeAttr",
+                          mlirAttributeIsAFuriosaMemoryTypeAttr)
+      .def_classmethod(
+          "get",
+          [](const nb::object &cls, MlirContext context,
+             mlir::furiosa::MemoryType value) {
+            return cls(mlirFuriosaMemoryTypeAttrGet(context, value));
+          },
+          nb::arg("cls"), nb::arg("context").none() = nb::none(),
+          nb::arg("value"));
 }
--- lib/CAPI/Dialect/Furiosa.cpp
+++ lib/CAPI/Dialect/Furiosa.cpp
@@ -26,3 +26,54 @@ MlirTypeID mlirFuriosaBufferTypeGetTypeID(void) {
 MlirType mlirFuriosaBufferTypeGet(MlirContext ctx) {
   return wrap(furiosa::BufferType::get(unwrap(ctx)));
 }
+
+//===---------------------------------------------------------------------===//
+// TargetAttr
+//===---------------------------------------------------------------------===//
+
+bool mlirAttributeIsAFuriosaTargetAttr(MlirAttribute attr) {
+  return isa<furiosa::TargetAttr>(unwrap(attr));
+}
+
+MlirTypeID mlirFuriosaTargetAttrGetTypeID(void) {
+  return wrap(furiosa::TargetAttr::getTypeID());
+}
+
+MlirAttribute mlirFuriosaTargetAttrGet(MlirContext ctx, std::uint64_t npu,
+                                       std::uint64_t peBegin,
+                                       std::uint64_t peEnd) {
+  return wrap(furiosa::TargetAttr::get(unwrap(ctx), npu, peBegin, peEnd));
+}
+
+//===---------------------------------------------------------------------===//
+// MappingAttr
+//===---------------------------------------------------------------------===//
+
+bool mlirAttributeIsAFuriosaMappingAttr(MlirAttribute attr) {
+  return isa<furiosa::MappingAttr>(unwrap(attr));
+}
+
+MlirTypeID mlirFuriosaMappingAttrGetTypeID(void) {
+  return wrap(furiosa::MappingAttr::getTypeID());
+}
+
+MlirAttribute mlirFuriosaMappingAttrGet(MlirContext ctx) {
+  return wrap(furiosa::MappingAttr::get(unwrap(ctx)));
+}
+
+//===---------------------------------------------------------------------===//
+// MemoryTypeAttr
+//===---------------------------------------------------------------------===//
+
+bool mlirAttributeIsAFuriosaMemoryTypeAttr(MlirAttribute attr) {
+  return isa<furiosa::MemoryTypeAttr>(unwrap(attr));
+}
+
+MlirTypeID mlirFuriosaMemoryTypeAttrGetTypeID(void) {
+  return wrap(furiosa::MemoryTypeAttr::getTypeID());
+}
+
+MlirAttribute mlirFuriosaMemoryTypeAttrGet(MlirContext ctx,
+                                           mlir::furiosa::MemoryType value) {
+  return wrap(furiosa::MemoryTypeAttr::get(unwrap(ctx), value));
+}
