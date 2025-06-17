c74077b4dba3f87300f9dc76e586693075167832
hyunmin-furiosa <hyunmin.jeong@furiosa.ai>
2025-06-17 08:27:47 +0000

    Separate execution engine

---
+++ include/furiosa-mlir/ExecutionEngine/ExecutionEngine.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <random>
+
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/Operation.h"
+
+#include "llvm/ADT/Any.h"
+#include "llvm/ADT/ArrayRef.h"
+#include "llvm/Support/Error.h"
+
+#include "furiosa-mlir/ExecutionEngine/RenegadeRuntime.h"
+
+namespace mlir::furiosa {
+
+class ExecutionEngine {
+public:
+  ExecutionEngine(Operation *module)
+      : module(module), randomNumberGenerator(), distribution() {}
+
+  static llvm::Expected<std::unique_ptr<ExecutionEngine>>
+  create(Operation *module);
+
+  llvm::Error invoke(StringRef func_name, StringRef func_type);
+
+  Operation *getModule() const { return module; }
+
+  void createValue(Value val, llvm::Any data) {
+    if (!valueMapper.count(val)) {
+      valueMapper.insert_or_assign(val, data);
+    }
+  }
+
+  /// get current data of value
+  llvm::Any &getValue(Value val) {
+    if (!valueMapper.count(val)) {
+      llvm::report_fatal_error(llvm::Twine("value does not exist"));
+    }
+    return valueMapper[val];
+  }
+
+  std::uint64_t getRandomNumber() {
+    return distribution(randomNumberGenerator);
+  }
+
+private:
+  using ValueMapper = llvm::DenseMap<Value, llvm::Any>;
+
+  Operation *module;
+
+  /// Map from value to its data
+  ValueMapper valueMapper;
+
+  /// random number generator
+  std::mt19937 randomNumberGenerator;
+  std::uniform_int_distribution<std::uint64_t> distribution;
+};
+
+} // namespace mlir::furiosa
--- include/furiosa-mlir/ExecutionEngine/RenegadeRuntime.h
+++ include/furiosa-mlir/ExecutionEngine/RenegadeRuntime.h
@@ -1,17 +1,13 @@
 #pragma once
 
-#include <random>
-
 #include "device_runtime.h"
 
-#include "mlir/IR/BuiltinOps.h"
-
-#include "llvm/ADT/Any.h"
-#include "llvm/ADT/ArrayRef.h"
-
+#include "furiosa-mlir/ExecutionEngine/ExecutionEngine.h"
 #include "furiosa-mlir/Target/C/FuriosaTaskToC.h"
 
 namespace mlir::furiosa {
+
+class ExecutionEngine;
 
 #define CEIL(a, b) (((a + b - 1) / b) * b)
 
@@ -23,45 +19,7 @@ using hal_program_t = SmallVector<device_runtime::Program *>;
 using device_t = device_runtime::Device *;
 using execution_t = device_runtime::Execution *;
 
-class ExecutionContext {
-public:
-  ExecutionContext(Operation *module)
-      : module(module), randomNumberGenerator(), distribution() {}
-
-  Operation *getModule() const { return module; }
-
-  void createValue(Value val, llvm::Any data) {
-    if (!valueMapper.count(val)) {
-      valueMapper.insert_or_assign(val, data);
-    }
-  }
-
-  /// get current data of value
-  llvm::Any &getValue(Value val) {
-    if (!valueMapper.count(val)) {
-      llvm::report_fatal_error(llvm::Twine("value does not exist"));
-    }
-    return valueMapper[val];
-  }
-
-  std::uint64_t getRandomNumber() {
-    return distribution(randomNumberGenerator);
-  }
-
-private:
-  using ValueMapper = llvm::DenseMap<Value, llvm::Any>;
-
-  Operation *module;
-
-  /// Map from value to its data
-  ValueMapper valueMapper;
-
-  /// random number generator
-  std::mt19937 randomNumberGenerator;
-  std::uniform_int_distribution<std::uint64_t> distribution;
-};
-
-LogicalResult executeFunction(Operation *module, StringRef entry_point,
+LogicalResult executeFunction(ExecutionEngine &engine, StringRef entry_point,
                               StringRef entry_point_type);
 
 } // namespace mlir::furiosa
