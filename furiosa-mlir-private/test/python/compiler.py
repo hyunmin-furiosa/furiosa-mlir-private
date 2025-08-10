198627ac7470bc252bda5296b2f82f02b6019ab1
hyunmin-furiosa <hyunmin.jeong@furiosa.ai>
2025-08-10 04:31:35 +0000

    Add test for python generated module

--- furiosa-mlir-private/test/python/compiler.py
+++ furiosa-mlir-private/test/python/compiler.py
@@ -4,7 +4,9 @@ from furiosa_mlir.execution_engine import *
 from furiosa_mlir.ir import *
 from furiosa_mlir.passmanager import *
 
+import furiosa_mlir.dialects.func as func
 import furiosa_mlir.dialects.furiosa as furiosa
+import furiosa_mlir.dialects.tosa as tosa
 
 def test_compiler_high_level():
     with Context() as ctx, Location.unknown():
@@ -55,5 +57,46 @@ module {
         expected = np.einsum("nij,njk->nik", arr0, arr1)
         print(np.array_equal(arr2, expected))
 
+def test_python_generated_module():
+    n = 64
+    i = 64
+    j = 64
+    k = 64
+
+    with Context() as ctx, Location.unknown():
+        i8_type = IntegerType.get_signless(8)
+        arr0_type = RankedTensorType.get((n, i, j), i8_type)
+        arr1_type = RankedTensorType.get((n, j, k), i8_type)
+        arr2_type = RankedTensorType.get((n, i, k), i8_type)
+        zp_type = RankedTensorType.get((1,), i8_type)
+        zp_element = IntegerAttr.get(i8_type, 0)
+        zp_attr = DenseElementsAttr.get([zp_element], zp_type)
+
+        module = Module.create()
+        function = func.FuncOp("kernel", ([arr0_type, arr1_type], [arr2_type]))
+        function.attributes["target"] = furiosa.TargetAttr.get(npu=0, pe_begin=0, pe_end=0)
+        function.add_entry_block()
+        v0 = tosa.ConstOp(zp_attr)
+        v1 = tosa.MatMulOp(arr2_type, function.arguments[0], function.arguments[1], v0, v0)
+        function.entry_block.append(v0)
+        function.entry_block.append(v1)
+        function.entry_block.append(func.ReturnOp(v1))
+        module.body.append(function)
+
+        compiler = Compiler()
+        compiler.compile(module)
+
+        arr0 = np.random.randint(0, 2, size=(n, i, j), dtype=np.int8)
+        arr1 = np.random.randint(0, 2, size=(n, j, k), dtype=np.int8)
+        arr2 = np.zeros((n, i, k), dtype=np.int8)
+
+        target = furiosa.TargetAttr.get(npu=0, pe_begin=0, pe_end=0)
+        execution_engine = ExecutionEngine(module, target)
+        execution_engine.invoke("kernel", [arr0, arr1], [arr2])
+
+        expected = np.einsum("nij,njk->nik", arr0, arr1)
+        print(np.array_equal(arr2, expected))
+
 test_compiler_high_level()
 test_compiler_and_execution_engine()
+test_python_generated_module()
