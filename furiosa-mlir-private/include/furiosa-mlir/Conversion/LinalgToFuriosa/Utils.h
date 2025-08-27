aba8732eb4a1c5d0839d6b07445e47d47d91a004
hyunmin-furiosa <hyunmin.jeong@furiosa.ai>
2025-08-27 02:12:59 +0000

    Add optimize indexers

--- furiosa-mlir-private/include/furiosa-mlir/Conversion/LinalgToFuriosa/Utils.h
+++ furiosa-mlir-private/include/furiosa-mlir/Conversion/LinalgToFuriosa/Utils.h
@@ -10,6 +10,7 @@
 namespace mlir::furiosa {
 
 static constexpr auto SLICE_SRAM_SIZE = 0x400000; // 4MB
+static constexpr auto DRAM_ACCESS_WIDTH = 256;
 using indexer_t =
     std::pair<SmallVector<std::int64_t>, SmallVector<std::int64_t>>;
 using indexers_t = std::pair<indexer_t, indexer_t>;
@@ -49,6 +50,49 @@ indexer_t getPartitioningIndexer(llvm::SmallVector<OpFoldResult> offsets,
   }
 
   return {partitioning_limits, partitioning_strides};
+}
+
+indexers_t optimizeIndexers(indexers_t indexers) {
+  auto [source_indexer, destination_indexer] = indexers;
+  SmallVector<indexer_t> indexers_vector = {source_indexer,
+                                            destination_indexer};
+  SmallVector<std::uint32_t> consecutive_dims;
+  SmallVector<std::uint32_t> consecutive_sizes;
+  for (auto indexer : indexers_vector) {
+    auto [limits, strides] = indexer;
+    auto rank = limits.size();
+    for (auto i = 1u; i < rank; i++) {
+      auto inner_size = limits[i - 1] * strides[i - 1];
+      if (strides[i] != inner_size) {
+        consecutive_dims.push_back(i - 1);
+        consecutive_sizes.push_back(inner_size);
+        break;
+      }
+    }
+  }
+
+  assert(consecutive_dims[0] == consecutive_dims[1]);
+  assert(consecutive_sizes[0] == consecutive_sizes[1]);
+  auto max_consecutive_dim =
+      *std::max_element(consecutive_dims.begin(), consecutive_dims.end());
+  auto max_consecutive_size =
+      *std::max_element(consecutive_sizes.begin(), consecutive_sizes.end());
+
+  for (auto &indexer : indexers_vector) {
+    auto &[limits, strides] = indexer;
+    limits.erase(limits.begin(), limits.begin() + max_consecutive_dim);
+    strides.erase(strides.begin(), strides.begin() + max_consecutive_dim);
+    if ((max_consecutive_size >= DRAM_ACCESS_WIDTH) &&
+        (max_consecutive_size % DRAM_ACCESS_WIDTH == 0)) {
+      limits.insert(limits.begin(), max_consecutive_size / DRAM_ACCESS_WIDTH);
+      strides.insert(strides.begin(), DRAM_ACCESS_WIDTH);
+    } else {
+      limits.insert(limits.begin(), max_consecutive_size);
+      strides.insert(strides.begin(), 1);
+    }
+  }
+
+  return std::make_pair(indexers_vector[0], indexers_vector[1]);
 }
 
 indexers_t getIndexers(RankedTensorType dram_type, RankedTensorType sram_type,
@@ -112,7 +156,7 @@ indexers_t getIndexers(RankedTensorType dram_type, RankedTensorType sram_type,
 
   indexer_t dram_indexer = {limits, dram_strides};
   indexer_t sram_indexer = {limits, sram_strides};
-  return {dram_indexer, sram_indexer};
+  return optimizeIndexers({dram_indexer, sram_indexer});
 }
 
 } // namespace mlir::furiosa
